e63a14a7effe34b08f72fc7af4aa1ce8b0f78464
Viorel Tamas <tamasvio@gmail.com>
2013-10-19 10:02:25 +0300

    Modified to my preferences

--- install-deps.sh
+++ install-deps.sh
@@ -3,7 +3,7 @@
 
 # https://github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+# bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
@@ -29,32 +29,3 @@ chmod +x ~/code/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
 # z binary is already referenced from .bash_profile
-
-
-# https://github.com/dronir/SpotifyControl
-# Spotify Controll Script
-cd ~/code
-git clone git://github.com/dronir/SpotifyControl.git
-
-# https://github.com/jeroenbegyn/VLCControl
-# VLC Controll Script
-cd ~/code
-git clone git://github.com/jeroenbegyn/VLCControl.git
-
-
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures
-# then start Photo Booth. It'll ask where to put the library.
-# put it in Dropbox/public
-
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
-
-
-# for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
-
-
-# chrome canary as default
-# on a mac you can set chrome canary as your default inside of Safari preferences :)
-
